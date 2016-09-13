25d140df73dd0f9f7356b21e9718ac42f1cabcd8
t-rev <trev.tkyle@gmail.com>
2016-09-13 00:01:40 -0500

    works on mobile now.

--- AndroidShowcase.WebUI/Scripts/product.js
+++ AndroidShowcase.WebUI/Scripts/product.js
@@ -1,24 +1,29 @@
 ﻿$(document).ready(function () {
 
-   $(document).on("click", ".deleteButton", function () {
-
+    $(document).on("tap", '[data-role="delete"]', function () {
+                
         var pid = $(this).closest("tr").data("productid");
         var uid = $(this).closest("tr").data("userid");
 
         $.ajax({
-            url: "Product/Delete",
+            url: "AndroidShowcase/Product/Delete",
             type: "post",
+            cache: false,
             data: { productid: pid, userid: uid },
             success: function (result) {
                 $("#productlist").html(result);
-            }
+            },
+            error: function (result){
+                alert(JSON.stringify(result))
+        }
         });
     });
 
-    $(document).on("click", ".editButton", function () {
+    $(document).on("tap", '[data-role="edit"]', function () {
         $.ajax({
-            url: "Product/Edit",
+            url: "AndroidShowcase/Product/Edit",
             type: "get",
+            cache: false,
             data: $("form").serialize(),
             success: function (result) {
                 $("#productlist").html(result);
